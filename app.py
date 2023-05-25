4c35b80f4ef6c92c0c6a3d18cd6159359957a314
Rahmat Satyawan <rahmat.stywan@gmail.com>
2023-05-25 18:43:51 +0700

    🎉 init:

---
+++ app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+
+st.set_page_config(
+    page_title="My first app 2",
+    layout="wide"
+    )
+
+st.markdown("# Welcome to my first app")
+
+st.markdown("## This is my first app")
+
+st.write("Here's our first attempt at using data to create a table:")
+
